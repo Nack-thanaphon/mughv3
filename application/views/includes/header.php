ba141801ecdd27202a421497a3cdc9e45f49303c
Nack Thanaphon <78144742+nackdev2535@users.noreply.github.com>
2022-08-08 20:24:05 +0700

    sent to Desktop

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -5,10 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Mugh | <?php echo isset($title) ? $title : 'Mahidol University Global Health' ; ?></title>
-        <meta property="og:image" content="https://info-Mugh.com/bos/<?php echo isset($img) ? $img : '' ; ?> ">
-  
+
+    <title>Mugh | <?php echo isset($title) ? $title : 'Mahidol University Global Health'; ?></title>
+    <meta property="og:image" content="https://info-Mugh.com/bos/<?php echo isset($img) ? $img : ''; ?> ">
 
     <link rel="stylesheet" href="<?php echo Base_url('vendor/fontawesome-free/css/all.min.css') ?>">
     <link rel="stylesheet" href="<?php echo Base_url('vendor/slickjs/slick/slick.css') ?>">
